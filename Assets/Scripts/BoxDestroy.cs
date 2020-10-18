58c44088cd1f779bad01bf4c38ee54804bf89042
DESKTOP-CCEB1KM\NCSOFT <joming1418@gmail.com>
2020-10-18 14:46:57 +0900

    Revert "boxdestroy 수정"
    This reverts commit f07e826b4d54e90d121ce7092ffb03d9a1c0c9f0.

--- Assets/Scripts/BoxDestroy.cs
+++ Assets/Scripts/BoxDestroy.cs
@@ -8,24 +8,13 @@ public class BoxDestroy : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (gameManager.mode == 0)
+        if (collision.gameObject.CompareTag("Box"))
         {
-            if (collision.gameObject.CompareTag("Box"))
-            {
-                if (collision.gameObject.GetComponent<Box>().weight <= 4.5f)
-                {
-                    gameManager.reputation -= 10;
-                }
-                Destroy(collision.gameObject);
-            }
-        }
-        else if (gameManager.mode == 1)
-        {
-            if (collision.gameObject.CompareTag("Box"))
+            if(collision.gameObject.GetComponent<Box>().weight <= 4.5f)
             {
                 gameManager.reputation -= 10;
-                Destroy(collision.gameObject);
             }
+            Destroy(collision.gameObject);
         }
     }
 }
